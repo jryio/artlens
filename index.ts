b7905d3a4fe1cd4bf44b76d4a369c5c025bc0439
Jacob Young <jacob@jry.io>
2023-09-26 13:56:29 -0600

    Make it work

--- index.ts
+++ index.ts
@@ -11,4 +11,30 @@
 //
 // https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html#-reference-lib-
 
-console.log("Hello via Bun!");
+const mainImage = document.getElementById("main-image")
+console.log({ mainImage });
+
+// transform-origin: center, top left, etc.
+// transform: (scale, translateX,Y,Z, rotate, etc.)
+const transitionDelay = 1000
+
+const transforms = [
+  { transform: "scale(0.75)", transformOrigin: "left" },
+  { transform: "scale(2)", transformOrigin: "right" },
+  { transform: "scale(1)", transformOrigin: "top" },
+]
+
+// Loop through transitions
+for (const [index, t] of transforms.entries()) {
+
+  if (mainImage) {
+    // Delay
+    setTimeout(() => {
+      mainImage.style.transformOrigin = t.transformOrigin
+      mainImage.style.transform = t.transform
+      mainImage.style.transitionDuration = "300ms"
+      mainImage.style.transitionTimingFunction = "ease-in"
+      console.log({ t })
+    }, index * transitionDelay)
+  }
+}
